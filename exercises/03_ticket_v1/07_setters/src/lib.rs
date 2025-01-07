b407198365760b4745ed7d307bd0aa79a84ac351
Thuyen Code <thuyencode@proton.me>
2025-01-07 16:15:09 +0700

    solution: 03_ticket_v1/07_setters

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,27 +11,17 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
+        let mut ticket: Ticket = Ticket {
+            title: "".into(),
+            description: "".into(),
+            status: "".into(),
+        };
 
-        Ticket {
-            title,
-            description,
-            status,
-        }
+        ticket.set_title(title);
+        ticket.set_description(description);
+        ticket.set_status(status);
+
+        ticket
     }
 
     pub fn title(&self) -> &String {
@@ -44,6 +34,38 @@ impl Ticket {
 
     pub fn status(&self) -> &String {
         &self.status
+    }
+
+    pub fn set_title(&mut self, title: String) {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+
+        self.title = title
+    }
+
+    pub fn set_description(&mut self, description: String) {
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+
+        self.description = description
+    }
+
+    pub fn set_status(&mut self, status: String) {
+        if !vec!["To-Do", "In Progress", "Done"].contains(&&*status) {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+
+        self.status = status
     }
 }
 
