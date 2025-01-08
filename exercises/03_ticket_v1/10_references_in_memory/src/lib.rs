0fcd01e8dc55b75865cc3309aa9d16e38938e523
Thuyen Code <thuyencode@proton.me>
2025-01-08 19:22:41 +0700

    solution: 03_ticket_v1/10_references_in_memory

--- exercises/03_ticket_v1/10_references_in_memory/src/lib.rs
+++ exercises/03_ticket_v1/10_references_in_memory/src/lib.rs
@@ -12,17 +12,15 @@ mod tests {
     use std::mem::size_of;
 
     #[test]
-    fn u16_ref_size() {
-        assert_eq!(size_of::<&u16>(), todo!());
-    }
+    fn u16_ref_size() { assert_eq!(size_of::<&u16>(), 8); }
 
     #[test]
     fn u64_mut_ref_size() {
-        assert_eq!(size_of::<&mut u64>(), todo!());
+        assert_eq!(size_of::<&mut u64>(), 8);
     }
 
     #[test]
     fn ticket_ref_size() {
-        assert_eq!(size_of::<&Ticket>(), todo!());
+        assert_eq!(size_of::<&Ticket>(), 8);
     }
 }
